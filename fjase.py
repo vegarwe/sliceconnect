1776a49c8df4e902fd689a9a7b72fefba076930f
Vegar Westerlund <vegarwe@gmail.com>
2016-11-06 14:17:48 +0100

    Pairing with passkey working

--- fjase.py
+++ fjase.py
@@ -9,14 +9,19 @@ from pc_ble_driver_py.exceptions    import NordicSemiException, IllegalStateExce
 from pc_ble_driver_py.ble_driver    import BLEDriverObserver, BLEUUIDBase, BLEUUID, BLEGapAddr, BLEGapConnParams, NordicSemiException
 from pc_ble_driver_py.ble_adapter   import BLEAdapter, BLEAdapterObserver, EvtSync
 
-from fjase_ble_driver import FjaseBLEDriver, FjaseBLEDriverObserver
+from fjase_ble_driver import FjaseBLEDriver, FjaseBLEDriverObserver, BLEGapSecParams, BLEGapSecKeyset, BLEGapSecKeyDist
 
 
-logger = logging.getLogger('fjase')
-logger.setLevel(logging.DEBUG)
-sh = logging.StreamHandler()
-sh.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
-logger.addHandler(sh)
+def logger_setup():
+    logger = logging.getLogger('fjase')
+    logger.setLevel(logging.DEBUG)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+    logging.getLogger().addHandler(sh)
+    return logger
+
+logger = logger_setup()
 
 class Fjase(object):
     def __init__(self, serial_port, baud_rate=115200):
@@ -50,6 +55,7 @@ class FjaseAdapter(FjaseBLEDriverObserver, BLEDriverObserver, BLEAdapterObserver
         self.conn_handle        = None
         self.adapter            = adapter
         self.notifications_q    = Queue.Queue()
+        self.event_q            = Queue.Queue()
         self.adapter.observer_register(self)
         self.adapter.driver.observer_register(self)
         self.adapter.driver.extended_observer_register(self)
@@ -69,22 +75,47 @@ class FjaseAdapter(FjaseBLEDriverObserver, BLEDriverObserver, BLEAdapterObserver
         self.conn_handle = self.evt_sync.wait('connected')
         if self.conn_handle is None:
             raise NordicSemiException('Timeout. Device not found.')
-        #logger.debug('BLE: Service Discovery...')
-        #self.adapter.service_discovery(conn_handle=self.conn_handle)
-        #logger.debug('BLE: Service Discovery done')
 
-        #params = BLEGattcWriteParams(BLEGattWriteOperation.write_req,
-        #                                   BLEGattExecWriteFlag.unused,
-        #                                   handle,
-        #                                   cccd_list,
-        #                                   0)
+    def service_discovery(self):
+        logger.debug('BLE: Service Discovery...')
+        self.adapter.service_discovery(conn_handle=self.conn_handle)
+        logger.debug('BLE: Service Discovery done')
 
+
+    def read_attr(self, attr_handle):
         logger.debug('BLE: Read...')
         self.adapter.driver.read(self.conn_handle, 0x0003)
         #self.adapter.driver.write_req(
-        logger.debug('BLE: Read done')
         time.sleep(1)
-        #logger.debug('BLE: Enabling Notifications')
+        logger.debug('BLE: Read done')
+
+    def pair(self):
+        sec_params = BLEGapSecParams(bond           = True,
+                                 mitm           = True,
+                                 le_sec_pairing = False,
+                                 keypress_noti  = False,
+                                 io_caps        = 4, # KeyboardDisplay
+                                 oob            = False,
+                                 min_key_size   = 16,
+                                 max_key_size   = 16,
+                                 kdist_own      = BLEGapSecKeyDist(),
+                                 #kdist_peer     = BLEGapSecKeyDist(enc_key=True))
+                                 kdist_peer     = BLEGapSecKeyDist())
+        self.adapter.driver.ble_gap_authenticate(self.conn_handle, sec_params)
+        sec_event = self.event_q.get(timeout=32)
+
+        key_set = BLEGapSecKeyset()
+        self.adapter.driver.ble_gap_sec_params_reply(self.conn_handle, 0, None, key_set)
+
+        evt, key_type = self.event_q.get(timeout=32)
+        if key_type == 0x01:
+            passkey = raw_input("pass key: ")
+            self.adapter.driver.ble_gap_auth_key_reply(self.conn_handle, key_type, map(ord, passkey))
+        else:
+            raise Exception("Unsupported auth key event")
+
+    def enable_notifications(self):
+        logger.debug('BLE: Enabling Notifications')
         #self.adapter.enable_notification(conn_handle=self.conn_handle, uuid=DFUAdapter.CP_UUID)
         #return self.target_device_name, self.target_device_addr
 
@@ -115,6 +146,21 @@ class FjaseAdapter(FjaseBLEDriverObserver, BLEDriverObserver, BLEAdapterObserver
     def on_gap_evt_adv_report(self, ble_driver, conn_handle, peer_addr, rssi, adv_type, adv_data):
         print "Hello", ble_driver, peer_addr.addr_type, peer_addr.addr, rssi, adv_type, adv_data
 
+    def on_gap_evt_sec_params_request(self, ble_driver, conn_handle, sec_params):
+        logger.info('BLE_GAP_EVT_SEC_PARAMS_REQUEST')
+        self.event_q.put(('BLE_GAP_EVT_SEC_PARAMS_REQUEST', sec_params))
+
+    def on_gap_evt_auth_key_request(self, ble_driver, conn_handle, key_type):
+        logger.info('BLE_GAP_EVT_AUTH_KEY_REQUEST %r', key_type)
+        self.event_q.put(('BLE_GAP_EVT_AUTH_KEY_REQUEST', key_type))
+
+    def on_gap_evt_conn_sec_update(self, ble_driver, conn_handle, sec_mode, sec_level, encr_key_size):
+        logger.info('BLE_GAP_EVT_CONN_SEC_UPDATE')
+
+    def on_gap_evt_auth_status(self, ble_driver, conn_handle, auth_status, error_src, bonded):
+        logger.info('BLE_GAP_EVT_AUTH_STATUS')
+
+
 
     def on_notification(self, ble_adapter, conn_handle, uuid, data):
         if self.conn_handle         != conn_handle: return
@@ -123,20 +169,27 @@ class FjaseAdapter(FjaseBLEDriverObserver, BLEDriverObserver, BLEAdapterObserver
         logger.debug(data)
         self.notifications_q.put(data)
 
+
+
     def on_gattc_evt_read_rsp(self, ble_driver, conn_handle, status, error_handle, attr_handle, offset, data):
         logger.info("Got read response conn_handle %s status %s err_handle %s attr_handle %s offset %s data: %r",
                 conn_handle, status, error_handle, attr_handle, offset, ''.join(map(chr, data)))
 
 def main():
-	ble_backend = Fjase(serial_port="COM4")
-	ble_backend.open()
-	#ble_backend.fjase_adapter.scan_start(timeout=.3)
-        ble_backend.fjase_adapter.connect(
-                BLEGapAddr(BLEGapAddr.Types.random_static, [0xD6, 0x60, 0xC4, 0xA9, 0x6B, 0x5F]))
-                #BLEGapAddr(BLEGapAddr.Types.random_static, [0xFE, 0xE4, 0x5D, 0xE9, 0x02, 0x19]))
-                #BLEGapAddr(BLEGapAddr.Types.random_static, [0xEA, 0x81, 0xE3, 0xD0, 0x09, 0xC2]))
-                #BLEGapAddr(BLEGapAddr.Types.random_static, [0xFB, 0x5E, 0xB7, 0xBD, 0xEC, 0x39]))
-        ble_backend.close()
+    ble_backend = Fjase(serial_port="COM4")
+    ble_backend.open()
+
+    #ble_backend.fjase_adapter.scan_start(timeout=.3)
+
+    ble_backend.fjase_adapter.connect(
+            BLEGapAddr(BLEGapAddr.Types.random_static, [0xD6, 0x60, 0xC4, 0xA9, 0x6B, 0x5F]))
+            #BLEGapAddr(BLEGapAddr.Types.random_static, [0xFE, 0xE4, 0x5D, 0xE9, 0x02, 0x19]))
+            #BLEGapAddr(BLEGapAddr.Types.random_static, [0xEA, 0x81, 0xE3, 0xD0, 0x09, 0xC2]))
+            #BLEGapAddr(BLEGapAddr.Types.random_static, [0xFB, 0x5E, 0xB7, 0xBD, 0xEC, 0x39]))
+    ble_backend.fjase_adapter.pair()
+    #ble_backend.fjase_adapter.service_discovery()
+    ble_backend.fjase_adapter.read_attr(0x0003)
+    ble_backend.close()
 
 if __name__ == '__main__':
 	main()
--- fjase_ble_driver.py
+++ fjase_ble_driver.py
@@ -1,12 +1,118 @@
 import logging
 import wrapt
+from types      import NoneType
 
 from pc_ble_driver_py.ble_driver import driver, util, BLEEvtID, BLEGattStatusCode, NordicSemiErrorCheck, BLEDriver, BLEDriverObserver
 
 logger = logging.getLogger('fjase')
 
+class BLEGapSecKeyDist(object):
+    def __init__(self, enc_key=False, id_key=False, sign_key=False, link_key=False):
+        self.enc_key    = enc_key
+        self.id_key     = id_key
+        self.sign_key   = sign_key
+        self.link_key   = link_key
+
+    @classmethod
+    def from_c(cls, kdist):
+        return cls(enc_key       = kdist.enc,
+                   id_key        = kdist.id,
+                   sign_key      = kdist.sign,
+                   link_key      = kdist.link)
+
+    def to_c(self):
+        kdist       = driver.ble_gap_sec_kdist_t()
+        kdist.enc   = self.enc_key
+        kdist.id    = self.id_key
+        kdist.sign  = self.sign_key
+        kdist.link  = self.link_key
+        return kdist
+
+class BLEGapSecParams(object):
+    def __init__(self, bond, mitm, le_sec_pairing, keypress_noti, io_caps, oob, min_key_size, max_key_size, kdist_own, kdist_peer):
+        self.bond           = bond
+        self.mitm           = mitm
+        self.le_sec_pairing = le_sec_pairing
+        self.keypress_noti  = keypress_noti
+        self.io_caps        = io_caps
+        self.oob            = oob
+        self.min_key_size   = min_key_size
+        self.max_key_size   = max_key_size
+        self.kdist_own      = kdist_own
+        self.kdist_peer     = kdist_peer
+
+    @classmethod
+    def from_c(cls, sec_params):
+        return cls(bond             = sec_params.bond,
+                   mitm             = sec_params.mitm,
+                   le_sec_pairing   = sec_params.lesc,
+                   keypress_noti    = sec_params.keypress,
+                   io_caps          = sec_params.io_caps,
+                   oob              = sec_params.oob,
+                   min_key_size     = sec_params.min_key_size,
+                   max_key_size     = sec_params.max_key_size,
+                   kdist_own        = BLEGapSecKeyDist.from_c(sec_params.kdist_own),
+                   kdist_peer       = BLEGapSecKeyDist.from_c(sec_params.kdist_peer))
+
+    def to_c(self):
+        sec_params              = driver.ble_gap_sec_params_t()
+        sec_params.bond         = self.bond
+        sec_params.mitm         = self.mitm
+        sec_params.lesc         = self.le_sec_pairing
+        sec_params.keypress     = self.keypress_noti
+        sec_params.io_caps      = self.io_caps
+        sec_params.oob          = self.oob
+        sec_params.min_key_size = self.min_key_size
+        sec_params.max_key_size = self.max_key_size
+        sec_params.kdist_own    = self.kdist_own.to_c()
+        sec_params.kdist_peer   = self.kdist_peer.to_c()
+        return sec_params
+
+class BLEGapSecKeyset(object):
+    # sd_ble_gap_sec_params_reply(uint16_t conn_handle, uint8_t sec_status, ble_gap_sec_params_t const *p_sec_params, ble_gap_sec_keyset_t const *p_sec_keyset));
+
+    def __init__(self, ):
+        pass #self.kdist_peer     = kdist_peer
+
+
+    @classmethod
+    def from_c(cls, sec_params):
+        raise NotImplemented()
+
+    def to_c(self):
+        sec_keyset              = driver.ble_gap_sec_keyset_t()
+        keys_own                = driver.ble_gap_sec_keys_t()
+        sec_keyset.keys_own     = keys_own
+
+        keys_peer               = driver.ble_gap_sec_keys_t()
+        peer_enc_key            = driver.ble_gap_enc_key_t()
+        peer_enc_key.enc_info   = driver.ble_gap_enc_info_t()
+        peer_enc_key.master_id  = driver.ble_gap_master_id_t()
+        keys_peer.p_enc_key     = peer_enc_key
+        #keys_peer.p_id_key      = driver.ble_gap_id_key_t()
+        #keys_peer.p_sign_key    = driver.ble_gap_sign_info_t()
+        #keys_peer.p_pk          = driver.ble_gap_lesc_p256_pk_t()
+        sec_keyset.keys_peer    = driver.ble_gap_sec_keys_t()
+
+        return sec_keyset
+
+
 class FjaseBLEDriverObserver(object):
+    # TODO: These seems to belong on ble_adapter, not ble_driver
     def on_gattc_evt_read_rsp(self, ble_driver, conn_handle, status, error_handle, attr_handle, offset, data):
+        # TODO: Does status and error_handle make sense for read_rsp
+        pass
+
+    def on_gap_evt_sec_params_request(self, ble_driver, conn_handle, sec_params):
+        pass
+
+    def on_gap_evt_auth_key_request(self, ble_driver, conn_handle, key_type):
+        pass
+
+    def on_gap_evt_conn_sec_update(self, ble_driver, conn_handle, sec_mode, sec_level, encr_key_size):
+        pass
+
+    def on_gap_evt_auth_status(self, ble_driver, conn_handle, auth_status, error_src, bonded):
         pass
 
 class FjaseBLEDriver(BLEDriver):
@@ -24,17 +130,44 @@ class FjaseBLEDriver(BLEDriver):
     def extended_observer_unregister(self, observer):
         self.extended_observers.remove(observer)
 
+
+
+
+    @NordicSemiErrorCheck
+    @wrapt.synchronized(BLEDriver.api_lock)
+    def ble_gap_authenticate(self, conn_handle, sec_params):
+        assert isinstance(sec_params, BLEGapSecParams), 'Invalid argument type'
+        return driver.sd_ble_gap_authenticate(self.rpc_adapter, conn_handle, sec_params.to_c())
+
+    @NordicSemiErrorCheck
+    @wrapt.synchronized(BLEDriver.api_lock)
+    def ble_gap_sec_params_reply(self, conn_handle, sec_status, sec_params, sec_keyset):
+        assert isinstance(sec_params, (BLEGapSecParams, NoneType)), 'Invalid argument type'
+        assert isinstance(sec_keyset, BLEGapSecKeyset), 'Invalid argument type'
+        if sec_params:
+            sec_params = sec_params.to_c()
+        return driver.sd_ble_gap_sec_params_reply(self.rpc_adapter,
+                conn_handle, sec_status, sec_params, sec_keyset.to_c())
+
+    @NordicSemiErrorCheck
+    @wrapt.synchronized(BLEDriver.api_lock)
+    def ble_gap_auth_key_reply(self, conn_handle, key_type, key):
+        key_buf = util.list_to_uint8_array(key)
+        return driver.sd_ble_gap_auth_key_reply(self.rpc_adapter,
+                conn_handle, key_type, key_buf.cast())
+
+
     @NordicSemiErrorCheck
     @wrapt.synchronized(BLEDriver.api_lock)
     def read(self, conn_handle, read_handle, offset=0):
-        #assert isinstance(write_params, BLEGattcWriteParams), 'Invalid argument type'
         return driver.sd_ble_gattc_read(self.rpc_adapter, conn_handle, read_handle, offset)
 
-    #def read(self):
-    #    driver.sd_ble_gattc_read(self.rpc_adapter, self.conn_handle, 0x0003, 0)
+
+
 
     def log_message_handler(self, adapter, severity, log_message):
-        pass#logger.info("log_message %s, %s", severity, log_message)
+        with open('log.txt', 'a') as logfile:
+            logfile.write('%s\n' % (log_message))
 
     def ble_evt_handler(self, adapter, ble_event):
         if not self._sync_extended_evt_handler(adapter, ble_event):
@@ -44,6 +177,7 @@ class FjaseBLEDriver(BLEDriver):
     @wrapt.synchronized(BLEDriver.observer_lock)
     def _sync_extended_evt_handler(self, adapter, ble_event):
         try:
+            #logger.info('ble_event %r', ble_event.header.evt_id)
             if ble_event.header.evt_id == driver.BLE_GATTC_EVT_READ_RSP:
                 read_rsp = ble_event.evt.gattc_evt.params.read_rsp
                 data = util.uint8_array_to_list(read_rsp.data, read_rsp.len)
@@ -55,6 +189,45 @@ class FjaseBLEDriver(BLEDriver):
                                               attr_handle  = read_rsp.handle,
                                               offset       = read_rsp.offset,
                                               data         = util.uint8_array_to_list(read_rsp.data, read_rsp.len))
+                return True
+            elif ble_event.header.evt_id == driver.BLE_GAP_EVT_SEC_PARAMS_REQUEST:
+                sec_params = ble_event.evt.gap_evt.params.sec_params_request.peer_params
+                for obs in self.extended_observers:
+                    obs.on_gap_evt_sec_params_request(ble_driver    = self,
+                                                      conn_handle   = ble_event.evt.gap_evt.conn_handle,
+                                                      sec_params    = BLEGapSecParams.from_c(sec_params))
+                return True
+            #elif ble_event.header.evt_id == driver.BLE_GAP_EVT_SEC_INFO_REQUEST:
+            #    logger.info('BLE_GAP_EVT_SEC_INFO_REQUEST')
+            #    return True
+            #elif ble_event.header.evt_id == driver.BLE_GAP_EVT_SEC_REQUEST:
+            #    logger.info('BLE_GAP_EVT_SEC_REQUEST')
+            #    return True
+            elif ble_event.header.evt_id == driver.BLE_GAP_EVT_AUTH_KEY_REQUEST:
+                auth_key_request = ble_event.evt.gap_evt.params.auth_key_request
+                for obs in self.extended_observers:
+                    obs.on_gap_evt_auth_key_request(ble_driver  = self,
+                                                    conn_handle = ble_event.evt.gap_evt.conn_handle,
+                                                    key_type    = auth_key_request.key_type)
+                return True
+            elif ble_event.header.evt_id == driver.BLE_GAP_EVT_CONN_SEC_UPDATE:
+                conn_sec = ble_event.evt.gap_evt.params.conn_sec_update.conn_sec
+                for obs in self.extended_observers:
+                    obs.on_gap_evt_conn_sec_update(ble_driver       = self,
+                                                   conn_handle      = ble_event.evt.gap_evt.conn_handle,
+                                                   sec_mode         = conn_sec.sec_mode.sm,
+                                                   sec_level        = conn_sec.sec_mode.lv,
+                                                   encr_key_size    = conn_sec.encr_key_size)
+                return True
+            elif ble_event.header.evt_id == driver.BLE_GAP_EVT_AUTH_STATUS:
+                auth_status = ble_event.evt.gap_evt.params.auth_status
+                for obs in self.extended_observers:
+                    obs.on_gap_evt_auth_status(ble_driver           = self,
+                                               conn_handle          = ble_event.evt.gap_evt.conn_handle,
+                                               auth_status          = auth_status.auth_status,
+                                               error_src            = auth_status.error_src,
+                                               bonded               = auth_status.bonded
+                                               )
                 return True
         except Exception as e:
             logger.error("Exception: {}".format(str(e)))
